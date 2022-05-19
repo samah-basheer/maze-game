187153e0f49eb94285bd163dea9c2f27f1cb62ee
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-19 13:35:48 +0300

    added conditions to restrict the rules of the game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,43 @@
 let lose = false;
 let score = 0;
+let start = 0;
+let end = false;
 
 function startGame() {
+    start = 1;
     document.getElementById('status').innerText = "Begin by moving your mouse over the \"S\".";
     lose = false;
     var boundaries = document.querySelectorAll('.boundary');
     for(var i = 0; i < boundaries.length; i ++) {
         boundaries[i].classList.remove("lose");
     }
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].onmouseover = overBoundaries;
+    }
 }
 
 function endGame() {
-    if(lose) {
-        score -= 10;
-        document.getElementById('status').innerText = "You Lost! Your Score is: "+ score;
-    } else {
-        score += 5;
-        document.getElementById('status').innerText = "You Won! Your Score is: " + score;
+    if(start == 1) {
+        if(lose) {
+            score -= 10;
+            document.getElementById('status').innerText = "You Lost! Your Score is: "+ score;
+        } else {
+            score += 5;
+            document.getElementById('status').innerText = "You Won! Your Score is: " + score;
+        }
+        start = 0;
+        end = true;
     }
 }
 
 function overBoundaries() {
-    lose = true;
-    var boundaries = document.querySelectorAll('.boundary');
-    for(var i = 0; i < boundaries.length; i ++) {
-        boundaries[i].classList.add("lose");
+    if(!end) {
+        lose = true;
+        document.getElementById('status').innerText = "You Lost!";
+        var boundaries = document.querySelectorAll('.boundary');
+        for(var i = 0; i < boundaries.length; i ++) {
+            boundaries[i].classList.add("lose");
+        }
     }
 }
 
@@ -33,8 +46,3 @@ startBtn.onclick = startGame;
 
 var endBtn = document.getElementById('end');
 endBtn.onmouseover = endGame;
-
-var boundaries = document.querySelectorAll('.boundary');
-for (var i = 0; i < boundaries.length; i++) {
-    boundaries[i].onmouseover = overBoundaries;
-}
